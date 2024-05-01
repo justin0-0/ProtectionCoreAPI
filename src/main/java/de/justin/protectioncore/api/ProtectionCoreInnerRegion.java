631a09f2fcb73c69301c6ac8857b77a528d5ea03
Justin-_- <61631935+justin0-0@users.noreply.github.com>
2024-05-01 19:00:00 +0200

    Add files via upload

---
+++ src/main/java/de/justin/protectioncore/api/ProtectionCoreInnerRegion.java
@@ -0,0 +1,42 @@
+package de.justin.protectioncore.api;
+
+import de.justin.protectioncore.util.Module;
+import de.justin.protectioncore.util.Region;
+import org.bukkit.Location;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class ProtectionCoreInnerRegion {
+    private Location location;
+    private ArrayList<UUID> members;
+    private ArrayList<UUID> allMembers;
+    private UUID owner;
+    private ArrayList<Module> modules;
+    private ArrayList<String> customModules;
+    private Location min;
+    private Location max;
+    public ProtectionCoreInnerRegion(Region region) {
+    }
+    public Location getLocation() {
+        return location.clone();
+    }
+    public ArrayList<UUID> getAllMembers() {return allMembers;}
+    public UUID getOwner() {
+        return owner;
+    }
+    public ArrayList<UUID> getMembers() {
+        return (ArrayList<UUID>) members.clone();
+    }
+    public ArrayList<Module> getModules() {
+        return (ArrayList<Module>) modules.clone();
+    }
+    public ArrayList<String> getCustomModules() {return (ArrayList<String>) customModules.clone();}
+    public Location getMin() {
+        return min.clone();
+    }
+    public Location getMax() {
+        return max.clone();
+    }
+}
---
+++ src/main/java/de/justin/protectioncore/util/Module.java
@@ -0,0 +1,24 @@
+package de.justin.protectioncore.util;
+
+
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+
+public enum Module {
+    PROTECTION_ONE(null),
+    PROTECTION_TWO(null),
+    PROTECTION_THREE(null),
+    PROTECTION_FOUR(null),
+    PVP(null),
+    CREEPER(null),
+    MOB(null),
+    NOTHING(null);
+
+    public ItemStack itemStack;
+    Module(ItemStack itemStack1) {itemStack1 = itemStack;}
+    public static Module getModuleByItem(ItemStack itemStack) {
+        //Just dummy Code
+        return MOB;
+    }
+}
+
